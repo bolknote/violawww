8f905ac40c4aab591e21c52b617b0aec30cff508
Evgeny Stepanischev <imbolk@gmail.com>
2025-11-07 16:14:03 +0300

    Fix deprecated non-prototype function declarations
    Add full function prototypes to cl_BCard.h and cl_generic.h to fix
    compiler warnings about deprecated K&R-style declarations without
    parameters. All function declarations now include complete parameter
    lists (VObj* self, Packet* result, int argc, Packet argv[]) to
    ensure C23 compatibility and eliminate deprecated-non-prototype
    warnings.

--- src/viola/cl_BCard.h
+++ src/viola/cl_BCard.h
@@ -5,12 +5,12 @@
 #include "cl_field.h"
 
 extern ClassInfo class_BCard;
-long helper_BCard_get();
-long helper_BCard_set();
+long helper_BCard_get(VObj* self, Packet* result, int argc, Packet argv[], long labelID);
+long helper_BCard_set(VObj* self, Packet* result, int argc, Packet argv[], long labelID);
 
-long meth_BCard_config();
-long meth_BCard_expose();
-long meth_BCard_get();
-long meth_BCard_initialize();
-long meth_BCard_render();
-long meth_BCard_set();
+long meth_BCard_config(VObj* self, Packet* result, int argc, Packet argv[]);
+long meth_BCard_expose(VObj* self, Packet* result, int argc, Packet argv[]);
+long meth_BCard_get(VObj* self, Packet* result, int argc, Packet argv[]);
+long meth_BCard_initialize(VObj* self, Packet* result, int argc, Packet argv[]);
+long meth_BCard_render(VObj* self, Packet* result, int argc, Packet argv[]);
+long meth_BCard_set(VObj* self, Packet* result, int argc, Packet argv[]);

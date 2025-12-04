8e28e21d864e28f08725251943ded5d55e296505
Evgeny Stepanischev <imbolk@gmail.com>
2025-12-04 11:26:00 +0300

    Remove debug prints and temp files

--- src/viola/embeds/HTML_point_script.v
+++ src/viola/embeds/HTML_point_script.v
@@ -7,13 +7,10 @@
 	case "D":
 		/* Send point to parent polygon */
 		p = parent();
-		print("[POINT D] parent()=", p, "\n");
 		if (p == "" || p == "0" || p == "(NULL)") {
 			p = send("HTML_polygon", "getCurrentPrimitive");
-			print("[POINT D] getCurrentPrimitive=", p, "\n");
 		}
 		if (p != "" && p != "0" && p != "(NULL)") {
-			print("[POINT D] sending addPoint(", pointX, ",", pointY, ") to ", p, "\n");
 			send(p, "addPoint", pointX, pointY);
 		}
 		return 0;
